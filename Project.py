4d449afb582fca7181796ddda894efd3d30e5822
Abhay Shivhare <abhayshihare2536@gmail.com>
2026-05-22 10:39:47 +0530

    fixed utf8mb4 emoji support

--- Project.py
+++ Project.py
@@ -35,6 +35,8 @@ app.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.environ.get("MYSQL_DB")
 app.config['MYSQL_PORT'] = int(os.environ.get("MYSQL_PORT", 3306))
+app.config['MYSQL_CHARSET'] = 'utf8mb4'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql=MySQL()
 mysql.init_app(app)
